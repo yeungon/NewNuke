a9ff6b1d1c0b917ff790784dcb84231937a4f95f
yeungon <yeungon@gmail.com>
2018-05-18 23:32:14 -0600

    fix app.com

--- app/core/App.php
+++ app/core/App.php
@@ -33,48 +33,10 @@ class App
 		/*sẽ được khởi tạo từ trong Router.php*/
 		//new \app\controllers\HomeController;
 
-		$this->router = new \app\core\Router\Router;
-		
-		$this->router->get('/{abc}/{cde}', 'HomeController@index');
+		/*khởi tạo Autoload*/
+		new \Autoload(self::$config['rootDir']);
 
-
-		$this->router->get('/abc', function (){
-
-			echo "nội dung từ trang abc";
-
-		});
-
-
-
-		$this->router->post('/test', function (){
-
-			echo "đây là trang test";
-
-		});
-
-		$this->router->any('/post/{category}/{id}/{bac}', function ($arg1, $arg2, $arg3){
-
-			echo "đây là trang post và có các paramater $arg1, và $arg2 và $arg3";
-
-		});
-
-		$this->router->any('/news/{abc}/{abc}', function ($arg1, $arg2){
-
-			echo "đây là trang $arg1, và $arg2";
-
-		});
-
-		$this->router->any('/admin', function (){
-
-			echo "Hello @admin đây là trang admin.";
-
-		});
-
-		/*catch lỗi*/;
-		$this->router->any("*", function(){
-			echo "400 not found";
-		});
-
+		$this->router = new Router(self::$config['basePath']);
 	}
 
 	/**
