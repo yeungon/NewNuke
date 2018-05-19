c1c697cc452b4e312f1168e4a2437d0586f52829
yeungon <yeungon@gmail.com>
2018-05-18 20:48:50 -0600

    test controller

---
+++ app/routers.php
@@ -0,0 +1,25 @@
+<?php
+	//use app\core\Controller;
+
+	
+	Router::get('/home',function(){
+		echo '<h2> hello from @home router home GET </h2>';
+	});
+
+
+	Router::get('/abc','HomeController@hello');
+	
+	/*Router::get('/',function(){
+		$ct = new Controller;
+		$ct->render('index',['age' => 22, 'name' => 'tai']);
+	});*/
+
+	Router::get('/',function(){
+		echo '<h1> hello đây index</h1>';
+	});
+
+
+	Router::any('*',function(){
+		echo '404';
+	});
+?>
