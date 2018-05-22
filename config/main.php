900de69a13e57bf5a820972c8e740e869ebc3c08
yeungon <yeungon@gmail.com>
2018-05-22 03:21:33 -0600

    add some other config

--- config/main.php
+++ config/main.php
@@ -1,10 +1,12 @@
 <?php
 
 return[
-	'basePath' => '/vietphpdev/public',
+	'basePath' 		=> '/vietphpdev/public',
+	'public' 		=>	$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
+	'rootpublic' 	=> dirname(__DIR__).'/public/',
 	/*thư mục gốc, dirname lồng nhau*/
-	'rootDir' => dirname(__DIR__),
-	'layout'  => 'layouts/main'
+	'rootDir' 		=> dirname(__DIR__),
+	'layout'  		=> 'layouts/main'
 
 ];
 
